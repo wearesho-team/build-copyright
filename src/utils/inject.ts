8dbeb9dfb548e831ad091720557d42d66cb88822
Alexander Letnikow <reclamme@gmail.com>
2019-11-26 00:38:35 +0200

    Fix inject

--- src/utils/inject.ts
+++ src/utils/inject.ts
@@ -4,7 +4,7 @@ import { BuildProcessEnv, Environment, Meta } from "../entities";
 export function inject(metaFile: string = "meta.json"): string[] {
     const meta = Meta(require(process.cwd() + "/" + metaFile));
     const env = BuildProcessEnv(meta);
-    console.log(messages.build(Environment(env)));
     Object.assign(process.env, env);
+    console.log(messages.build(Environment(env)));
     return Object.keys(env);
 }
